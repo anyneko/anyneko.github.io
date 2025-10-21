2f68cf8c74cbfefd55d3873927cc4b41ecda1a54
Misaka13514 <Misaka13514@gmail.com>
2025-10-21 20:30:37 +0800

    [+] Add RSS Plugin
    https://www.11ty.dev/docs/plugins/rss/

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+import { feedPlugin } from "@11ty/eleventy-plugin-rss";
+
 export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/styles.css");
 
@@ -43,7 +45,27 @@ export default function(eleventyConfig) {
       a.localeCompare(b, "zh-Hans", { sensitivity: "base" }),
     );
   });
-  
+
+  // RSS Feed Plugin
+  eleventyConfig.addPlugin(feedPlugin, {
+    type: "atom", // or "rss", "json"
+    outputPath: "/feed.xml",
+    collection: {
+      name: "post", // iterate over `collections.post`
+      limit: 0,     // 0 means no limit
+    },
+    metadata: {
+      language: "zh",
+      title: "Ameyama Mio's Blog",
+      subtitle: "死掉了就太可惜啦。陪妳多走一程，好不好？",
+      base: "https://aneko.moe/",
+      author: {
+        name: "eko",
+        email: "", // Optional
+      }
+    }
+  });
+
   return {
     dir: {
       input: "src",
